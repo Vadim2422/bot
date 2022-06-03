c75298fed1cbc7d376041559c666e7cb20d12883
Vadim2422 <breev.vadim@yandex.ru>
2022-06-03 22:53:40 +0300

    v1

---
+++ bot_b.py
@@ -0,0 +1,59 @@
+from email import message
+import API
+from vkbottle.bot import Bot, Message
+
+post=[]
+bot = Bot(token=API.API)
+@bot.on.chat_message(text ="start")
+async def message_handler(message: Message):
+    await message.answer("кабан")
+
+def read_file():
+    list=[]
+    f=open('base.txt','r')
+    for line in f:
+        list.append(line)
+    print(list)
+    f.close()
+    return list
+
+
+@bot.on.message(text="Добавить на пост <item>")
+async def message_handler(message: Message, item=None):
+    if item is not None:
+        if post.count(item)!=0:
+            await message.answer("Уже на посту ")
+        else:
+            post.append(item)
+            await message.answer("Пост пидора принял "+item)
+
+
+@bot.on.message(text=["Кто на посту?","Кто на посту"])
+async def message_handler(message: Message):
+    if len(post):
+        if len(post)==1:
+            str="Пост держит:\n"
+        else:
+            str="Пост держат:\n"
+        for line in post:
+            str+=line+"\n"
+        await message.answer(str)
+    else:
+        await message.answer("На посту никого")
+
+@bot.on.message(text="Удалить с поста <item>")
+async def message_handler(message: Message,item=None):
+    if item is not None:
+        if post.count(item):
+            post.remove(item)
+            await message.answer("Пост сдал "+item)
+        else:
+            await message.answer(item+" и так не пидор")
+
+
+
+@bot.on.message(text="/help")
+async def message_handler(message: Message):
+    await message.answer("1) Кто на посту\n2) Добавить на пост\n3) Удалить с поста\n(Без нумерации)")
+
+bot.run_forever()
